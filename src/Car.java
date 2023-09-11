640fbe72c630d53a84829524d934e5c1589e240f
JohnHilld <yfdsyfds0@ukr.net>
2023-09-11 04:08:48 +0300

    Local to cloud

---
+++ src/Car.java
@@ -0,0 +1,20 @@
+public class Car {
+
+    public void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startElectricity() {
+        //TODO turn on main switch
+    }
+
+    private void startCommand() {
+        //TODO start computer
+    }
+
+    private void startFuelSystem() {
+        //TODO start fuel pump
+    }
+}
---
+++ src/Package2/SameName.java
@@ -0,0 +1,5 @@
+package Package2;
+
+public class SameName {
+    //Second class with same name
+}
